8ad3829c10a58a409d18cd4e8661a67b87077cd8
Tom Forwood <tom-git@forwoods.org>
2018-04-08 18:50:51 +0100

    Creates some initial node types

---
+++ MHNodes/ActionKillWarp.cs
@@ -0,0 +1,50 @@
+﻿using Expansions.Missions;
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace MHNodes
+{
+    public class ActionKillWarp : ActionModule
+    {
+        public string Name
+        {
+            get { return "Kill warp"; }
+        }
+
+        public new String GetDisplayName()
+
+        {
+            String s = base.GetDisplayName();
+            return Name + " " + s;
+        }
+
+        public override string GetInfo()
+        {
+            return "A node which simply kills the timewarp when hit";
+        }
+
+        public override IEnumerator Fire()
+        {
+            Debug.Log("Fire is being called");
+            TimeWarp.SetRate(0, false, false);
+            return Enumerable.Empty<int>().GetEnumerator();
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Load(ConfigNode node)
+        {
+            base.Load(node);
+        }
+
+        public override void Save(ConfigNode node)
+        {
+            base.Save(node);
+        }
+    }
+}
---
+++ MHNodes/TestPartPreent.cs
@@ -0,0 +1,68 @@
+﻿using Expansions.Missions;
+using Expansions.Missions.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MHNodes
+{
+    class TestPartPresent : TestModule
+    {
+        [MEGUI_InputField(guiName = "TestPartName")]
+        public String testPartName;
+
+        [MEGUI_PartPicker(guiName = "TestPart")]
+        public System.Object testPart;
+
+        public TestPartPresent()
+        {
+        }
+
+
+        public override string GetInfo()
+        {
+            return "Tests whether the active vessel has a given part";
+        }
+
+        public override void Load(ConfigNode node)
+        {
+            base.Load(node);
+            testPartName = node.GetValue("PartName");
+        }
+
+        public override void Save(ConfigNode node)
+        {
+            base.Save(node);
+            node.AddValue("PartName", testPartName);
+        }
+
+        public override void Awake()
+        {
+            Debug.Log("I'm awake");
+            base.Awake();
+        }
+
+        public override string GetNodeBodyParameterString(BaseAPField field)
+        {
+            Debug.Log("Getting MBP for " + field.FieldType);
+            return base.GetNodeBodyParameterString(field);
+        }
+
+        public override bool Test()
+        {
+            //Debug.Log("Running test for part" + testPart.partName);
+            Vessel v = FlightGlobals.ActiveVessel;
+            Debug.Log(v.GetDisplayName());
+            Debug.Log(v.parts.Count());
+            foreach (Part p in v.Parts)
+            {
+                Debug.Log("Path name = " + p.partName);
+                Debug.Log("PartInfoName = " + p.partInfo.name);
+                Debug.Log("AVailPart = " + p.partInfo.name);
+            }
+            return v.Parts.Exists((Part p) => p.partInfo.name == testPartName);
+        }
+    }
+}
